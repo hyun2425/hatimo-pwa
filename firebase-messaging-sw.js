3a5aff8b21e20671e8d7e57057e4db06c930b302
hyun2425 <hyun24.dev@gmail.com>
2025-04-12 23:00:50 +0900

    Updates

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,24 @@
+// public/firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js')
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js')
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyAF6Cr7C67Es81G5kifmO4QELdTla1P-kk',
+  projectId: 'hatimo-e23ef',
+  messagingSenderId: '827641897840',
+  appId: '1:827641897840:web:bfa7f7a58e9dc58b2425b5',
+})
+
+
+const messaging = firebase.messaging()
+
+messaging.onBackgroundMessage(function (payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload)
+  const notificationTitle = payload.notification.title
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/pwa-icon-192.png'
+  }
+
+  self.registration.showNotification(notificationTitle, notificationOptions)
+})
